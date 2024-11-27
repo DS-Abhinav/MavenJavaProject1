9dc60fcd74df4bbf09ff09d702ab9beefe47e442
DS-Abhinav <unqabhinav@gmail.com>
2024-11-27 11:12:50 +0530

    Update App.java

--- src/main/java/SEGroup/MavenJavaProject1/App.java
+++ src/main/java/SEGroup/MavenJavaProject1/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+         System.out.println( "22bd1a664g" );
+         System.out.println( "poll demo" );
+        
     }
 }
